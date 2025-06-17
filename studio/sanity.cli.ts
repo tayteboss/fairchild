64e8a01668e80fecd6cfbf2cbbdd3d56dc8e64e2
Tayte Boss <boss.tayte@gmail.com>
2025-06-17 16:40:53 +0100

    style: Package updates

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'b356qmqr',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
